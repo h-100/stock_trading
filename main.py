cc4dd6419ff3de1e74a990ca53ea9261713dfca4
h-100 <hira.javaid008@gmail.com>
2024-05-21 22:19:12 +0500

    add a single stock info in a table with strategy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import argparse
+import pdb
 
+import pandas as pd
 from backtesting import Backtest
+from tabulate import tabulate
 
 import DataHandler as dh
 import strategies.RsiOscillator as rsiOsc
@@ -26,12 +29,34 @@ def get_list_of_tickers(num):
   """
   
   pass
+def convert_to_dataframe(stats):
+  # df = pd.DataFrame()
+  # df.columns = ['Start', 'End', 'Duration', 'Exposure Time', 'Equity Final [$]', 'Equity Peak [$]', 'Return [%]', 'Return (Ann.) [%]', 'Volatility (Ann.) [%]']
+
+  data = {}
+  col_names = ['Start', 'End', 'Duration', 'Exposure Time [%]', 'Equity Final [$]', 'Equity Peak [$]', 'Return [%]']
+
+  for i in col_names:
+    data[i] = []
+
+  # pdb.set_trace()
+  for i in range(len(stats)):
+    for key in data.keys():
+      data[key].append(stats[i][key])
+
+  return pd.DataFrame(data)
 
 def print_stats(stats):
+  
   """
   print stats in a printable format
   """
-  pass
+  df = convert_to_dataframe(stats)
+  print(df.to_markdown())
+
+  # if not df.empty:
+  #   print(df.to_markdown())
+  convert_to_dataframe(stats)
 
 def get_plot():
   pass
@@ -47,12 +72,13 @@ def main():
   parser.add_argument('-t', '--ticker', action="store", dest="ticker")
 
   args = parser.parse_args()
+  global_stats = []
 
   ### get data 
   data = dh.DataHandler(args.ticker, args.start_date, args.end_date).load_data()
-
   stats = get_stats(args.strategy_name, data, args.cash)
-  print(stats)
+  global_stats.append(stats)
+  print_stats(global_stats)
   
 
 
@@ -61,6 +87,8 @@ if __name__ == "__main__":
   main()
 
 ## Priority TODOS
+####### add handlers for flags, are they required or are they not required
+####### add help indicating how to run everything
 #### 1. TODO1: transform stats to columns and print in the form of a table 
 #### 2. TODO2: do the same for multiple number of stocks 
 #### 3. TODO3: add other strategies 
